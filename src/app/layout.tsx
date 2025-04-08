b73e90b7ad9de099b9e5d820cc3b6a192850e750
Ryan Rawlings <rjrawlings@gmail.com>
2025-04-08 18:18:33 -0400

    target content section of the page more specifically in the CSS.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        <div className="container">
+        <div className="container content">
           {children}
         </div>
         <Footer />
