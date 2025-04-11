21cee3226b5fe8ef0df7df48284be89852189739
Ryan Rawlings <rjrawlings@gmail.com>
2025-04-11 12:57:25 -0400

    move stuff that's not markdown related out of blog-post

--- src/components/blog-post.tsx
+++ src/components/blog-post.tsx
@@ -4,27 +4,16 @@ import remarkMath from "remark-math";
 import { Post } from "@/utils/posts";
 import remarkGfm from "remark-gfm";
 
-export function BlogPost({data}: { data: Post }) {
+export function BlogPost({ data }: { data: Post }) {
   const content = data.content;
   const date = data.published_at.toISOString().split("T")[0];
 
-
   return (
-    <>
-      <h2>{data.title}</h2>
-      <time dateTime={date}>
-        {date}
-      </time>
-      <h3>{data.snippet}</h3>
-
-      <Markdown
-        remarkPlugins={[remarkMath, remarkGfm]}
-        rehypePlugins={[rehypeKatex]}
-      >
-        {content}
-      </Markdown>
-    </>
-
-  )
-
+    <Markdown
+      remarkPlugins={[remarkMath, remarkGfm]}
+      rehypePlugins={[rehypeKatex]}
+    >
+      {content}
+    </Markdown>
+  );
 }
