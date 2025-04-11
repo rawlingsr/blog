10c4bd17f874c9613368a554525bcae965c09c60
Ryan Rawlings <rjrawlings@gmail.com>
2025-04-11 13:01:07 -0400

    assign plugins based on type of post.

--- src/components/blog-post.tsx
+++ src/components/blog-post.tsx
@@ -4,14 +4,38 @@ import remarkMath from "remark-math";
 import { Post } from "@/utils/posts";
 import remarkGfm from "remark-gfm";
 
+function remarkPlugins(data: Post) {
+  let plugins = [];
+
+  if (data.math) {
+    plugins.push(remarkMath);
+  }
+
+  if (data.gfm) {
+    plugins.push(remarkGfm);
+  }
+
+  return plugins;
+}
+
+function rehypePlugins(data: Post) {
+  let plugins = [];
+
+  if (data.math) {
+    plugins.push(rehypeKatex);
+  }
+
+  return plugins;
+}
+
 export function BlogPost({ data }: { data: Post }) {
   const content = data.content;
   const date = data.published_at.toISOString().split("T")[0];
 
   return (
     <Markdown
-      remarkPlugins={[remarkMath, remarkGfm]}
-      rehypePlugins={[rehypeKatex]}
+      remarkPlugins={remarkPlugins(data)}
+      rehypePlugins={rehypePlugins(data)}
     >
       {content}
     </Markdown>
