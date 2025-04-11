3d7ab87a1c02a02f09dfe0945869985af680861d
Ryan Rawlings <rjrawlings@gmail.com>
2025-04-11 10:05:39 -0400

    rename post ot blog-post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { BlogPost } from "@/components/post";
+import { BlogPost } from "@/components/blog-post";
 import { getPost } from "@/utils/posts";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
