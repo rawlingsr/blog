9b8386f6100be0bdc8c72532444464af84eff578
Ryan Rawlings <rjrawlings@gmail.com>
2025-04-15 13:31:47 -0400

    remove font awesome from the header, where it it never used

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 
 export function Header() {
   return (
